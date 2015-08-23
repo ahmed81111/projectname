c48eb6f8ae14b2eb07780fbc57da55984def4c17
ahmed81111 <ahmed81111@gmail.com>
2015-08-23 19:21:38 +0530

    initial commit

---
+++ app/src/main/java/com/example/dell/buttonevent/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.dell.buttonevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+//the button
+    public void clickme(View v){
+
+        if(v.getId()==R.id.button)
+        {Log.d("ahm","1st button clicked"+v.getId());}
+        else if(v.getId()==R.id.button2)
+        {Log.d("ahm","2nd button is clicked"+v.getId());}
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

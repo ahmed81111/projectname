8ef1e4709b52ee2c20def8190d6e72351f5bb61a
ahmed81111 <ahmed81111@gmail.com>
2015-08-23 19:47:34 +0530

    button was clicked via view OnClickListner interface

--- app/src/main/java/com/example/dell/buttonevent/MainActivity.java
+++ app/src/main/java/com/example/dell/buttonevent/MainActivity.java
@@ -6,13 +6,18 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+Button button,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         button= (Button) findViewById(R.id.button);
+        button2= (Button) findViewById(R.id.button2);
+        button.setOnClickListener(this);
+        button2.setOnClickListener(this);
     }
 
     @Override
@@ -43,5 +48,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    Log.d("ahm","button was clicked   "+ view.getId());
+if(view.getId()==R.id.button){
+    Log.d("ahm","button 1 was clicked");
+
+}
+        else {
+    Log.d("ahm","button 2 was clicked");
+        }
+
     }
 }
